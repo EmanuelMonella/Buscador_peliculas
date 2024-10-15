03d30173d19147d9958d9c00abfbd6fde3c00784
EmanuelMonella <emanuel.monella@alumnos.ispi4038.edu.ar>
2024-10-15 16:53:27 -0300

    "nuevas pantallas desde qt "

--- ventana_actor.py
+++ ventana_actor.py
@@ -1,33 +1,24 @@
-from PySide6.QtWidgets import QWidget, QMessageBox, QVBoxLayout, QLabel, QLineEdit, QPushButton, \
-    QListWidget
+from PySide6.QtWidgets import QWidget, QMessageBox, QListWidget
+from pantalla_actor import Ui_actor  # Asegúrate de que este import sea correcto
+
 
 class VentanaBuscarPorActor(QWidget):
     def __init__(self, catalogo):
         super().__init__()
-        self.setWindowTitle("Buscar Películas por Actor")
-        self.setGeometry(100, 100, 300, 200)
+        self.ui = Ui_actor()
+        self.ui.setupUi(self)
         self.catalogo = catalogo
-        layout = QVBoxLayout()
-        self.label_actor = QLabel("Ingrese los nombres de los actores (separados por coma):")
-        layout.addWidget(self.label_actor)
-        self.input_actor = QLineEdit()
-        layout.addWidget(self.input_actor)
-        self.boton_buscar_actor = QPushButton("Buscar")
-        self.boton_buscar_actor.clicked.connect(self.buscar_actor)
-        layout.addWidget(self.boton_buscar_actor)
-        self.lista_resultados_actor = QListWidget()
-        layout.addWidget(self.lista_resultados_actor)
-        self.setLayout(layout)
 
-    def buscar_actor(self):
-        actores_input = self.input_actor.text().strip().lower()
-        self.lista_resultados_actor.clear()
+    def __buscar_actor(self):
+        actores_input = self.ui.input_busqueda.text().strip().lower()
+        self.ui.resultados.clear()  # Limpiar resultados del QListWidget
         if actores_input:
             actores = [actor.strip() for actor in actores_input.split(',')]
-            coincidencias = [p for p in self.catalogo if all(any(actor in a.lower() for a in p.get('actores', [])) for actor in actores)]
+            coincidencias = [p for p in self.catalogo if
+                             all(any(actor in a.lower() for a in p.get('actores', [])) for actor in actores)]
             if coincidencias:
                 for pelicula in coincidencias:
-                    self.lista_resultados_actor.addItem(pelicula['titulo'])
+                    self.ui.resultados.addItem(pelicula['titulo'])  # Añadir películas al QListWidget
             else:
                 QMessageBox.information(self, "Resultado", "No se encontraron películas para esos actores.")
         else:
