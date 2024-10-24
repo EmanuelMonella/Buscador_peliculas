b3c497e1399623427a8edc8f277c242232752f95
EmanuelMonella <emanuel.monella@alumnos.ispi4038.edu.ar>
2024-10-24 16:26:41 -0300

    Subiendo ventans

---
+++ ui/ventana_actor.py
@@ -0,0 +1,25 @@
+from PySide6.QtWidgets import QWidget, QMessageBox, QListWidget
+from ui.pantalla_actor import Ui_actor  # Asegúrate de que este import sea correcto
+
+
+class VentanaBuscarPorActor(QWidget):
+    def __init__(self, catalogo):
+        super().__init__()
+        self.ui = Ui_actor()
+        self.ui.setupUi(self)
+        self.catalogo = catalogo
+
+    def __buscar_actor(self):
+        actores_input = self.ui.input_busqueda.text().strip().lower()
+        self.ui.resultados.clear()  # Limpiar resultados del QListWidget
+        if actores_input:
+            actores = [actor.strip() for actor in actores_input.split(',')]
+            coincidencias = [p for p in self.catalogo if
+                             all(any(actor in a.lower() for a in p.get('actores', [])) for actor in actores)]
+            if coincidencias:
+                for pelicula in coincidencias:
+                    self.ui.resultados.addItem(pelicula['titulo'])  # Añadir películas al QListWidget
+            else:
+                QMessageBox.information(self, "Resultado", "No se encontraron películas para esos actores.")
+        else:
+            QMessageBox.warning(self, "Advertencia", "Por favor, ingrese los nombres de los actores.")
---
+++ ui/ventana_principal.py
@@ -0,0 +1,45 @@
+import json
+from PySide6.QtWidgets import QWidget, QMessageBox,QListWidgetItem
+from ui.buscador import Ui_Buscador
+from ui.ventana_actor import VentanaBuscarPorActor
+from ui.ventana_info import VentanaInfo
+
+class BuscadorPeliculas(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Buscador()
+        self.ui.setupUi(self)
+        self.__catalogo = self.__cargar_catalogo()
+
+    def __cargar_catalogo(self):
+        with open('peliculas/catalogo.json', 'r') as file:
+            return json.load(file)
+
+    def __buscar_pelicula(self):
+        nombre_pelicula = self.ui.input_pelicula.text().strip().lower()
+        self.ui.lista_resutado.clear()
+        if nombre_pelicula:
+            coincidencias = [p for p in self.__catalogo if nombre_pelicula in p['titulo'].lower()]
+            if coincidencias:
+                for pelicula in coincidencias:
+                    item = QListWidgetItem(pelicula['titulo'])
+                    item.setData(1, pelicula)
+                    self.ui.lista_resutado.addItem(item)
+            else:
+                QMessageBox.information(self, "Resultado", "No se encontraron coincidencias.")
+        else:
+            QMessageBox.warning(self, "Advertencia", "Por favor, ingrese un nombre de película.")
+
+    def __mostrar_info_pelicula(self):
+        item = self.ui.lista_resutado.currentItem()
+        if item and isinstance(item, QListWidgetItem):
+            pelicula = item.data(1)
+            if pelicula:
+                self.ventana_info = VentanaInfo(pelicula)
+                self.ventana_info.show()
+        else:
+            QMessageBox.warning(self, "Advertencia", "Por favor, seleccione una película.")
+
+    def __abrir_busqueda_por_actor(self):
+        self.ventana_actor = VentanaBuscarPorActor(self.__catalogo)
+        self.ventana_actor.show()
