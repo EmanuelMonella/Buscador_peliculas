5bf8d9bdf36d909bf69120207c7a71ec3db5360b
EmanuelMonella <emanuel.monella@alumnos.ispi4038.edu.ar>
2024-10-29 15:44:59 -0300

    'colocacion del mismo tamaño en todos los posters  '

--- ui/ventana_info.py
+++ ui/ventana_info.py
@@ -1,6 +1,8 @@
 from PySide6.QtWidgets import QWidget
 from PySide6.QtGui import QPixmap
 from ui.pantalla_resultado import Ui_info_peliculas
+from PySide6.QtCore import QSize
+
 
 class VentanaInfo(QWidget):
     def __init__(self, pelicula):
@@ -12,6 +14,8 @@ class VentanaInfo(QWidget):
         self.ui.resultado_actores.setText(actores)
         self.ui.resultado_sinopsis.setPlainText(pelicula.sinopsis)
         self.ui.resultado_puntuacion.setText(str(pelicula.puntuacion))
+
         if pelicula.poster:
+            self.ui.label_poster.setFixedSize(QSize(200, 300))
             self.ui.label_poster.setPixmap(QPixmap(pelicula.poster))
             self.ui.label_poster.setScaledContents(True)
